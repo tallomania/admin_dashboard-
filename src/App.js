3e880cbcaf3b05995dd6d23039c0403e07b5aecf
tallomania <haryorlee4christ@gmail.com>
2022-10-03 16:43:09 +0100

    Deleted some files

--- src/App.js
+++ src/App.js
@@ -70,6 +70,7 @@ const App = () => {
                             <Route path='/stacked' element={< Stacked />} />
                         </Routes>
                         </div>
+                        < Footer />
                     </div>
 
                 </div>
