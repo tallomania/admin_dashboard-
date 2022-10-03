a387d78a53a725df0609f122baba7ce2c60bf54a
tallomania <haryorlee4christ@gmail.com>
2022-10-03 16:53:54 +0100

    more component

---
+++ src/components/Charts/Pie.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { AccumulationChartComponent, AccumulationSeriesDirective, AccumulationSeriesCollectionDirective, Inject, AccumulationDataLabel, AccumulationLegend, PieSeries, AccumulationTooltip, } from "@syncfusion/ej2-react-charts";
+
+import { useStateContext } from '../../contexts/ContextProvider';
+
+import { Header } from '../../components';
+
+
+const Pie = ({ id, data, legendVisiblity, height }) => {
+    const { currentMode } = useStateContext();
+
+    return (
+        <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl' >
+            <Header category="Area" title="Inflation Rate in Pie" />
+            <AccumulationChartComponent
+                id={id}
+                legendSettings={{ visible: legendVisiblity, background: 'white' }}
+                height={height}
+                background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+                tooltip={{ enable: true }}
+            >
+                <Inject services={[AccumulationLegend, PieSeries, AccumulationDataLabel, AccumulationTooltip]} />
+                <AccumulationSeriesCollectionDirective>
+                    <AccumulationSeriesDirective
+                        name="Sale"
+                        dataSource={data}
+                        xName="x"
+                        yName="y"
+                        innerRadius="40%"
+                        startAngle={0}
+                        endAngle={360}
+                        radius="70%"
+                        explode
+                        explodeOffset="10%"
+                        explodeIndex={2}
+                        dataLabel={{
+                            visible: true,
+                            name: 'text',
+                            position: 'Inside',
+                            font: {
+                                fontWeight: '600',
+                                color: '#fff',
+                            },
+                        }}
+                    />
+                </AccumulationSeriesCollectionDirective>
+            </AccumulationChartComponent>
+        </div>
+    )
+}
+
+export default Pie;
