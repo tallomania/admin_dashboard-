5c85f66440178ee5ce0f6f6a637034ce359db06b
tallomania <haryorlee4christ@gmail.com>
2022-10-10 14:42:22 +0100

    Removed data

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -57,15 +57,7 @@ const Chat = () => {
                         </div>
                     </div>
                 ))}
-                <div className='mt-5' >
-                    <Button
-                        color='white'
-                        bgColor={currentColor}
-                        text='See all messages'
-                        borderRadius='10px'
-                        width='full'
-                    />
-                </div>
+                
             </div>
         </div>
     );
