ba1789119a5d46b6cfa47fb1f473f8d70c3de685
tallomania <haryorlee4christ@gmail.com>
2022-10-10 14:27:15 +0100

    Added more data

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs';
-import { Header } from '../components';
+import { Header } from '../components'
 
 const change = (args) => {
     document.getElementById('preview').style.backgroundColor = args.currentValue.hex;
 }
+const ColorPicker = () => {
+
+    return (
+        <div className='m-2 md:m-10 p-2 md:p-10 mt-24 bg-white rounded-3xl' >
+            <Header category='App' title='Color Picker' />
+            <div className='text-center' >
+                <div id='preview' />
+                    
+            </div>
+        </div>
+    )
+}
+
+export default ColorPicker;
