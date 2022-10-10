bb471b69495bcccdc19e5c5d613abbd7f6a8ab7a
tallomania <haryorlee4christ@gmail.com>
2022-10-10 14:28:58 +0100

    Added more data

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -12,7 +12,18 @@ const ColorPicker = () => {
             <Header category='App' title='Color Picker' />
             <div className='text-center' >
                 <div id='preview' />
-                    
+                <div className='flex justify-center items-center gap-20 flex-wrap' >
+                        <div>
+                            <p className='text-2xl font-semibold mt-2 mb-4' >Inline Pallete</p>
+                            <ColorPickerComponent
+                                id='inline-pallete'
+                                mode='Pallete'
+                            
+                                />
+                            </div>
+
+                    </div>
+                </div>
             </div>
         </div>
     )
