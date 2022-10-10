82b3e8f2a2249c82816a28c911945075ffdc6584
tallomania <haryorlee4christ@gmail.com>
2022-10-10 14:23:56 +0100

    Added more data

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -8,7 +8,9 @@ const Calendar = () => {
         <div className='m-2 md:m-10 p-2 md:p-10 mt-24 bg-white rounded-3xl' >
             <Header category='App' title='Calendar' />
             <ScheduleComponent
-               
+                height='650px'
+                eventSettings={{ dataSource: scheduleData }}
+                selectedDate={new Date(2021, 0, 10)}
             >
                 <Inject services={[ Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop ]} />
             </ScheduleComponent>
