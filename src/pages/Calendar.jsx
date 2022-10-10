e0bf2749ce81f4a8f81fa7d9acb570e53589e1be
tallomania <haryorlee4christ@gmail.com>
2022-10-10 14:22:04 +0100

    Added more data

---
+++ src/pages/Calendar.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop } from '@syncfusion/ej2-react-schedule';
+import { scheduleData } from '../data/dummy';
+import { Header } from '../components';
