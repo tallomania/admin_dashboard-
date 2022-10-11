d797ebf9bc069cdb69076f0be7023bf88fa950ac
tallomania <haryorlee4christ@gmail.com>
2022-10-11 18:34:19 +0100

    more data

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -7,7 +7,13 @@ const Employees = () => {
     return (
         <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl' >
             <Header category='Page' title='Employees' />
-            <GridComponent>
+            <GridComponent
+                dataSource={employeesData}
+                allowPaging
+                allowSorting
+                toolbar={['Search']}
+                width='auto'
+            >
                 <ColumnsDirective>
                     {employeesGrid.map((item, index) => (
                     <ColumnDirective key={index} {...item} />
