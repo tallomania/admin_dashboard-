ed49f1bc84454afd957fddf4e26d40f4ad5c3126
tallomania <haryorlee4christ@gmail.com>
2022-10-11 18:34:07 +0100

    more data

---
+++ src/pages/Employees.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { GridComponent, ColumnDirective, ColumnsDirective, Page, Toolbar, Inject, Search } from '@syncfusion/ej2-react-grids';
+import {employeesData, employeesGrid } from '../data/dummy';
+import { Header } from '../components';
+
+const Employees = () => {
+    return (
+        <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl' >
+            <Header category='Page' title='Employees' />
+            <GridComponent>
+                <ColumnsDirective>
+                    {employeesGrid.map((item, index) => (
+                    <ColumnDirective key={index} {...item} />
+                ))}
+                </ColumnsDirective>
+                <Inject services={[Page, Search, Toolbar]} />
+            </GridComponent>
+        </div>
+    )
+}
+
+export default Employees;
