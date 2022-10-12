ba03f88982e3c90c577dd3fbcbcf1ff796734ab1
tallomania <haryorlee4christ@gmail.com>
2022-10-12 13:23:40 +0100

    Added more data

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -69,7 +69,18 @@ const Ecommerce = () => {
                             </p>
                         </div>
                     </div>
-                   
+                    <div className='mt-10 flex gap-10 flex-wrap justify-center' >
+                        <div className='border-r-1 border-color m-4 pr-10' >
+                            <div>
+                                <p>
+                                    <span className='text-3xl font-semibold' > 98,538</span>
+                                    <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-gradient-to-bl from-cyan-300 to-green-600 ml-3 text-xs' >23%</span>
+                                </p>
+                                
+                            </div>
+
+
+                        </div>
 
                     </div>
                 </div>
