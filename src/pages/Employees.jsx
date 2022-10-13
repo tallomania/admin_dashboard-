4b425a00f3554b63eff065fd26ad50918bde793f
tallomania <haryorlee4christ@gmail.com>
2022-10-13 14:37:28 +0100

    Debugging

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -5,13 +5,13 @@ import { Header } from '../components';
 
 const Employees = () => {
     return (
-        <div className='m-2 md:m-10 p-2 md:p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl' >
+        <div className='m-2 md:m-10 p-2 md:p-10 dark:bg-secondary-dark-bg bg-white rounded-3xl' >
             <Header category='Page' title='Employees' />
             <GridComponent
                 dataSource={employeesData}
                 allowPaging
-                allowSorting
                 enableStickyHeader={true}
+                allowSorting
                 toolbar={['Search']}
                 width='auto'
             >
--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -5,7 +5,7 @@ import { Header } from '../components';
 
 const Kanban = () => {
     return (
-        <div className='m-2 md:m-10 p-2 md:p-10 mt-24 bg-white dark:bg-secondary-dark-bg rounded-3xl' >
+        <div className='m-2 md:m-10 p-2 md:p-10 mt-24 dark:bg-secondary-dark-bg bg-white rounded-3xl' >
             <Header category='App' title='Kanban' />
             <KanbanComponent
                 id='kanban'
