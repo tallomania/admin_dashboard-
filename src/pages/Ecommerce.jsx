e8e72e43279fd7306a2b209112d54702648d7588
tallomania <haryorlee4christ@gmail.com>
2022-10-16 15:07:33 +0100

    Added more data

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -173,7 +173,10 @@ const Ecommerce = () => {
                     </div>
                     </div>
                     <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl md:w-400 p-8 m-3 flex justify-center items-center gap-10 ' >
-
+                        <div>
+                            <p> $50,558 </p>
+                            <p>Yearly Sales </p>
+                        </div>
                     </div>
                 </div>
             </div>
