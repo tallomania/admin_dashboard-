c7e78bfcb2d39655cba56e1f0c9d23e8a53754c5
tallomania <haryorlee4christ@gmail.com>
2022-10-16 15:25:01 +0100

    Added more data

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -198,7 +198,7 @@ const Ecommerce = () => {
                         <DropDown currentMode={currentMode}  />
                     </div>
 
-                    <div>
+                    <div className='m-10 w-72 md:w-400' >
                         {recentTransactions.map((item) => (
                             <div key={item.title} className='flex justify-between mt-4' >
                                 <div>
