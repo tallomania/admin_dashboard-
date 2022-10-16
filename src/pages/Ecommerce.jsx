b7c6eae4b4e64cf05576b5c3c89ccc7eada15938
tallomania <haryorlee4christ@gmail.com>
2022-10-16 16:16:59 +0100

    Added more data

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -272,8 +272,12 @@ const Ecommerce = () => {
                                         <p className="text-sm text-gray-400">{item.desc}</p>
                                     </div>
                                 </div>
+                                <p className={`text-${item.pcColor}`}>{item.amount}</p>
                             </div>
-                        ) )}
+                        ))}
+                        <div className="mt-4">
+                            <SparkLine currentColor={currentColor} id="area-sparkLine" height="160px" type="Area" data={SparklineAreaData} width="320" color="rgb(242, 252, 253)" />
+                        </div>
                     </div>
                 </div>
             </div>
