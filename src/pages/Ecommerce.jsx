42d26a17654c13b2f25504ebc161357df9832391
tallomania <haryorlee4christ@gmail.com>
2022-10-16 16:22:13 +0100

    Added more data

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -280,6 +280,16 @@ const Ecommerce = () => {
                         </div>
                     </div>
                 </div>
+                <div className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
+                <div className="flex justify-between">
+                    <p className="text-xl font-semibold">MedicalPro Branding</p>
+                    <button type="button" className="text-xl font-semibold text-gray-400">
+                    <IoIosMore />
+                    </button>
+                </div>
+                
+
+
             </div>
         </div>
     )
