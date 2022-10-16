80df09b092237503e646ee6ee1603715833f4c1f
tallomania <haryorlee4christ@gmail.com>
2022-10-16 16:02:16 +0100

    Added more data

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -246,8 +246,8 @@ const Ecommerce = () => {
 
             <div className='flex flex-wrap justify-center' >
                 <div className='md:w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3' >
-                    <div>
-                        <p>Weekly Stats</p>
+                    <div className='flex justify-between' >
+                        <p className='text-xl font-semibold' >Weekly Stats</p>
                     </div>
                 </div>
             </div>
